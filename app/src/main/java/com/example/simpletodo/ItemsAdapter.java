8d860c6350c3d1bf76fa04fff3ef2ebe296314e2
Sara <sara.darensbourg@gmail.com>
2020-01-29 16:37:02 -0500

    Simple Todo for CodePath

---
+++ app/src/main/java/com/example/simpletodo/ItemsAdapter.java
@@ -0,0 +1,16 @@
+package com.example.simpletodo;
+
+import android.view.View;
+
+public class ItemsAdapter extends RecyclerView.Adapter{
+
+
+    //Container to provide easy access to views that represent each row of the list
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(@NonNull View itemView){
+            super(itemView);
+        }
+
+    }
+}
